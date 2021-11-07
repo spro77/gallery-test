37e59348565e3d3a4264908c3093602982f5e073
Sergey <Prostov>
2021-11-07 14:08:47 +0200

    axios adde

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+const App = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-export default App;
+  useEffect(() => {
+    axios("http://jsonplaceholder.typicode.com/photos")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+  }); }, []);
+
+  if (loading) return "Loading...";
+  if (error) return "Error!";
+  return ( <>
+    <img src={data[10].thumbnailUrl} alt={data[10].title} />
+    </>
+  )
+}
+export default App
